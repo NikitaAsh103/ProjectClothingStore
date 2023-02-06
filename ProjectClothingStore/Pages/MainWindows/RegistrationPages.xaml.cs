479bfaf811265a4757f63d350b08dd4692b7f606
Некит Ащеулов <xeek1337@gmail.com>
2023-02-06 10:50:41 +0300

    Делаю табуляцию

--- ProjectClothingStore/Pages/MainWindows/RegistrationPages.xaml.cs
+++ ProjectClothingStore/Pages/MainWindows/RegistrationPages.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProjectClothingStore.ClassHelper;
+using ProjectClothingStore.Windows;
+using ProjectClothingStore.DB;
 
 namespace ProjectClothingStore.Pages.MainWindows
 {
@@ -23,8 +26,96 @@ namespace ProjectClothingStore.Pages.MainWindows
         public RegistrationPages()
         {
             InitializeComponent();
+
+            CmbGender.ItemsSource = ClassHelper.EFclass.Contexts.Gender.ToList();
+            CmbGender.SelectedIndex = 0;
+            CmbGender.DisplayMemberPath = "GenderName";
         }
 
-      
+        private void BtnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TbLogin.Text))
+            {
+                MessageBox.Show("Поле Логин должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(TbEmail.Text))
+            {
+                MessageBox.Show("Поле Логин должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(TbLastName.Text))
+            {
+                MessageBox.Show("Поле Логин должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(TbFirsName.Text))
+            {
+                MessageBox.Show("Поле Логин должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(TbPatronymic.Text))
+            {
+                MessageBox.Show("Поле Логин должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(TbPhoneNumber.Text))
+            {
+                MessageBox.Show("Поле Логин должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(CmbGender.Text))
+            {
+                MessageBox.Show("Поле Пола должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(DPDateOfBirthday.Text))
+            {
+                MessageBox.Show("Поле Даты Рождения должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(PbPassword.Password))
+            {
+                MessageBox.Show("Поле Пароль должно быть заполнено", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            
+            
+
+
+            EFclass.Contexts.User.Add(new User()
+            {
+                Login = TbLogin.Text,
+                Password = PbPassword.Password,
+                LastName = TbLastName.Text, 
+                FirstName = TbFirsName.Text,
+                Patronymic= TbPatronymic.Text,  
+                Email= TbEmail.Text,    
+                PhoneNumber=TbPhoneNumber.Text, 
+                Birthday = DPDateOfBirthday.SelectedDate.Value,
+                IDGender = (CmbGender.SelectedItem as Gender).IDGender,
+            });
+
+            EFclass.Contexts.SaveChanges();
+            MessageBox.Show("OK");
+
+        }
+
+        private void TbLogin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TbLogin.Text== "Введите Логин")
+            {
+                TbLogin.Text = "";
+            }
+        }
+
+        private void TbLogin_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TbLogin.Text=="")
+            {
+                TbLogin.Text = "Введите Логин";
+            }
+        }
     }
 }
